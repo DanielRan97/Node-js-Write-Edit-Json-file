59a1c6ae6d47a09de5af8cc6a0e7bb91e5622644
DanielRan97 <daniel.daniel.ran@gmail.com>
2020-12-16 18:27:12 +0200

    Write&update json File

--- 1-json.js
+++ 1-json.js
@@ -2,16 +2,40 @@ const fs = require('fs')
 const chalk = require('chalk');
 const successLog = chalk.green.inverse.bold;
 
-
-const book = {
-
-    title:"Ego is the Enemy",
-    author:'Rayn Holiday'
+const user = {
+    name: "max",
+    planet: "Earth",
+    age: 33
 }
 
-const bookJson = JSON.stringify(book);
 
-fs.writeFileSync('1-json.json', bookJson);
+//function writeJsinFile(){
+// const bookJson = JSON.stringify(user);
+//
+// fs.writeFileSync('1-json.json', bookJson);
+//
+// console.log(successLog('Json is ready!'));
+//
+//}
 
-console.log(successLog('Json is ready!'));
+
+function updateJson(name,age){
+    const dataBuffer = fs.readFileSync('1-json.json');
+    const dataJson = dataBuffer.toString();
+    const data = JSON.parse(dataJson);
+    data.name = name;
+    data.age = age;
+    const dataToStrinf = JSON.stringify(data)
+    fs.writeFileSync('1-json.json',dataToStrinf)
+    console.log(successLog('json update to :' + dataToStrinf));
+}
+
+//writeJsinFile()
+updateJson('daniel',23);
+
+
+
+
+
+
 
